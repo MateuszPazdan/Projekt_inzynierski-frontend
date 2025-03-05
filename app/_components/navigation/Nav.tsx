1af203dc1508961e6b1a1a317a6e50206ba29de4
Mateusz Pazdan <pazdan.mateuszz@gmail.com>
2025-03-05 12:11:51 +0100

    burger btn

--- app/_components/navigation/Nav.tsx
+++ app/_components/navigation/Nav.tsx
@@ -1,5 +1,9 @@
+'use client';
+
 import { Gemunu_Libre } from 'next/font/google';
 import Image from 'next/image';
+import Burger from './Burger';
+import { useState } from 'react';
 
 const gemunu = Gemunu_Libre({
 	subsets: ['latin'],
@@ -7,9 +11,10 @@ const gemunu = Gemunu_Libre({
 });
 
 export default function Nav() {
+	const [isOpen, setIsOpen] = useState(false);
 	return (
 		<div className='h-16 sticky top-0 bg-white border-b-[2px] border-l-grayOne'>
-			<div className='max-w-[1800px] h-full mx-auto flex flex-row items-center px-5'>
+			<div className='max-w-[1800px] h-full mx-auto flex flex-row items-center justify-between px-5'>
 				<div className='flex flex-row gap-2 items-center'>
 					<div className='relative h-12 w-12'>
 						<Image
@@ -19,7 +24,17 @@ export default function Nav() {
 							className='aspect-square '
 						/>
 					</div>
-					<p className={`${gemunu.className} text-4xl text-main`}>Asset Flow</p>
+					<p className={`${gemunu.className} text-4xl text-main hidden`}>
+						Asset Flow
+					</p>
+				</div>
+				<div>
+					<Burger
+						isOpen={isOpen}
+						onClick={() => {
+							setIsOpen((isOpen) => !isOpen);
+						}}
+					/>
 				</div>
 			</div>
 		</div>
