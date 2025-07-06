49f693697a2d7a624e518092b75facdeb139c4aa
Mateusz Pazdan <pazdan.mateuszz@gmail.com>
2025-07-06 14:46:09 +0200

    holdingchangeChart

--- app/_components/invest/HoldingsChangeChart.tsx
+++ app/_components/invest/HoldingsChangeChart.tsx
@@ -1,6 +1,9 @@
 'use client';
 
 import {
+	Area,
+	AreaChart,
+	Brush,
 	CartesianGrid,
 	Line,
 	LineChart,
@@ -11,40 +14,123 @@ import {
 } from 'recharts';
 
 const data = [
-	{ time: '29.06', pv: 60984.23 },
-	{ time: '30.06', pv: 61203.56 },
-	{ time: '01.07', pv: 60000.45 },
-	{ time: '02.07', pv: 59810.12 },
-	{ time: '03.07', pv: 60234.89 },
-	{ time: '04.07', pv: 61012.77 },
-	{ time: '05.07', pv: 61540.67 },
+	{ time: '2024-06-29', price: 1605.23 },
+	{ time: '2024-06-30', price: 1600.23 },
+	{ time: '2024-07-01', price: 1604.23 },
+	{ time: '2024-07-02', price: 1605.23 },
+	{ time: '2024-07-03', price: 1606.23 },
+	{ time: '2024-07-04', price: 1607.23 },
+	{ time: '2024-07-05', price: 1606.23 },
 ];
 
 export default function HoldingsChangeChart() {
 	return (
-		<div className='flex flex-col rounded-lg border border-grayThird shadow-md  bg-white p-3 px-5'>
+		<div className='flex flex-col rounded-lg border border-grayThird shadow-md  bg-white p-3 px-5 overflow-hidden'>
 			<p className='text-xl font-medium mb-2'>Całkowita zmiana</p>
-			<ResponsiveContainer width='100%' height={300}>
-				<LineChart
+			<ResponsiveContainer width='100%' height='100%'>
+				<AreaChart
 					data={data}
-					margin={{
-						top: 50,
-						right: 20,
-						left: 20,
-						bottom: 50,
-					}}
+					margin={{ top: 20, right: 50, left: 50, bottom: 20 }}
+					height={300}
 				>
-					<XAxis dataKey='time' axisLine={false} tickMargin={30} />
-					<YAxis axisLine={false} unit='zł' tickMargin={10} />
-					<Tooltip />
 					<CartesianGrid vertical={false} />
-					<Line
-						type='monotone'
-						dataKey='pv'
-						stroke='#3c37ff'
-						activeDot={{ r: 8 }}
+					<XAxis
+						dataKey='time'
+						axisLine={false}
+						tickMargin={5}
+						tickFormatter={(currData) => {
+							const date = new Date(currData);
+							return date.toLocaleDateString('pl-PL', {
+								day: '2-digit',
+								month: 'short',
+							});
+						}}
 					/>
-				</LineChart>
+					<YAxis
+						dataKey={'price'}
+						axisLine={false}
+						tickLine={false}
+						domain={['dataMin', 'auto']}
+						tickMargin={10}
+						scale={'auto'}
+						orientation={'right'}
+						tickFormatter={(value) => {
+							if (value >= 1_000_000) {
+								const mln = value / 1_000_000;
+								const mlnLabel = Number.isInteger(mln)
+									? `${mln}`
+									: `${mln.toPrecision(3)}`;
+								return `${mlnLabel}\u00A0mln\u00A0zł`;
+							}
+
+							return Number(value).toLocaleString('pl-PL', {
+								style: 'currency',
+								currency: 'PLN',
+								currencySign: 'standard',
+								minimumFractionDigits: 0,
+								useGrouping: true,
+							});
+						}}
+					/>
+
+					<Tooltip
+						content={({ active, payload, label }) => {
+							if (active && payload && payload.length) {
+								const date = new Date(label as string);
+								const formattedDate = date.toLocaleDateString('pl-PL', {
+									weekday: 'long',
+									year: 'numeric',
+									month: 'long',
+									day: 'numeric',
+								});
+
+								return (
+									<div className='rounded-lg border border-grayThird shadow-md  bg-white px-4 py-2 text-sm'>
+										<p className='font-medium'>{formattedDate}</p>
+										<p>
+											Wartość:{' '}
+											{payload[0].value.toLocaleString('pl-PL', {
+												style: 'currency',
+												currency: 'PLN',
+												minimumFractionDigits: 2,
+											})}
+										</p>
+									</div>
+								);
+							}
+
+							return null;
+						}}
+					/>
+
+					<Area
+						type='linear'
+						dataKey='price'
+						name='Wartość portfela'
+						stroke='#3c37ff'
+						fillOpacity={1}
+						fill='url(#colorUv)'
+						strokeWidth={2}
+						activeDot={{ r: 5 }}
+					/>
+					<defs>
+						<linearGradient id='colorUv' x1='0' y1='0' x2='0' y2='1'>
+							<stop offset='10%' stopColor='#3c37ff' stopOpacity={0.7} />
+							<stop offset='90%' stopColor='#3c37ff' stopOpacity={0.1} />
+						</linearGradient>
+					</defs>
+					<Brush height={30} strokeWidth={1} stroke='#3e37ff'>
+						<LineChart>
+							<Line
+								type='linear'
+								dataKey='price'
+								stroke='#3c37ff'
+								strokeWidth={1}
+								dot={false}
+							/>
+						</LineChart>
+					</Brush>
+				</AreaChart>
 			</ResponsiveContainer>
 		</div>
 	);
--- app/_components/invest/TotalHoldingsChart.tsx
+++ app/_components/invest/TotalHoldingsChart.tsx
@@ -15,7 +15,7 @@ const data = [
 export default function TotalHoldingsChart() {
 	return (
 		<div className='flex flex-col rounded-lg border border-grayThird shadow-md bg-white p-3 px-5'>
-			<p className='text-xl font-medium mb-2'>Szacowana wartość</p>
+			<p className='text-xl font-medium mb-2'>Całkowite udziały</p>
 			<ResponsiveContainer width='100%' height={300}>
 				<PieChart>
 					<Pie
